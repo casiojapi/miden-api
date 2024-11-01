4430550bc0e8da01b315dc12debb74cc2de02aa0
Francisco Bezzecchi <franbcki@gmail.com>
2024-10-31 23:03:33 -0300

    feat: using serde, better routes

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,43 @@
 extern crate rocket;
 
 use errors::ApiError;
+use rocket::serde::json::{json, Json, Value};
+use rocket::serde::{Deserialize, Serialize};
 use wrapper::CliWrapper;
-
 
 mod errors;
 mod helpers;
-mod wrapper;
 mod txinfo;
+mod wrapper;
 
 #[get("/ping")]
 fn ping() -> &'static str {
     "pong"
 }
 
-#[get("/<username>/new", rank = 2)]
-async fn new(username: &str) -> Result<String, ApiError> {
-    let client = CliWrapper::new(username.into());
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct UserCreate {
+    username: String,
+}
+
+#[post("/create", format = "json", data = "<user>", rank = 2)]
+fn new_user(user: Json<UserCreate>) -> Result<Value, ApiError> {
+    let username = user.username.to_string();
+    let client = CliWrapper::new(username.clone());
     client.init_user()?;
     let account = client.create_account()?;
-    //let (note_id, _) = client.faucet_request(100).await?;
-    //client.consume_and_sync(&note_id).await?;
-    Ok(account)
+    Ok(json!({"username": username, "balance": 0, "address": account}))
 }
+
+#[get("/<username>/info", rank = 2)]
+fn get_user(username: &str) -> Result<Value, ApiError> {
+    let client = CliWrapper::new(username.into());
+    let account = client.create_account()?;
+    let balance = client.get_account_balance()?;
+    Ok(json!({"username": username, "balance": balance, "address": account}))
+}
+
 
 #[get("/<username>/faucet", rank = 2)]
 async fn faucet_fund(username: &str) -> Result<String, ApiError> {
@@ -42,11 +57,11 @@ async fn get_balance(username: &str) -> Result<String, ApiError> {
     Ok(balance)
 }
 
-#[get("/<username>/tx", rank = 2)]
-    async fn get_history(username: &str) -> Result<String, ApiError> {
+#[get("/<username>/transactions", rank = 2)]
+async fn get_history(username: &str) -> Result<String, ApiError> {
     let client = CliWrapper::from_username(username.into()).await?;
     client.init_user()?;
-    let data =client.sql_get_transactions();
+    let data = client.sql_get_transactions();
     Ok(data)
 }
 
@@ -63,14 +78,32 @@ async fn send_note(username: &str, to: &str, asset: &str) -> Result<String, ApiE
     Ok(note_id)
 }
 
-fn get_notes() {}
-
 #[get("/<username>/note/receive/<note_id>", rank = 2)]
 fn receive_note(username: &str, note_id: &str) {}
 
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Usernames {
+    usernames: Vec<String>
+}
+
+#[get("/usernames", rank = 2)]
+fn get_users() -> Json<Usernames> {
+    let usernames: Vec<String> = vec![
+        "mocked",
+        "fulano", "mengano", "sutano"
+    ].into_iter().map(|x| x.to_string()).collect();
+    Json(Usernames { usernames })
+}
+
 #[launch]
 fn run() -> _ {
-    rocket::build().mount("/", routes![ping])
-        .mount( "/account", routes![new, send_note, faucet_fund, get_balance, get_history],
+    rocket::build().mount("/", routes![ping]).mount(
+        "/api/account",
+        routes![
+        new_user,
+        get_user,
+        send_note, faucet_fund, get_balance, get_history, get_users],
     )
 }
